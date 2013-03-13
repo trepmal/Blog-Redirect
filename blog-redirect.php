4f2470d2d6956f6294512e0b329724b509dd57a9
Kailey Lampert <trepmal@gmail.com>
2013-03-12 23:27:33 -0700

    general cleanup

--- blog-redirect.php
+++ blog-redirect.php
@@ -6,31 +6,35 @@ Author: Kailey Lampert
 Author URI: http://kaileylampert.com/
 */
 
-$blogredirect = new blogredirect();
+$blog_redirect = new Blog_Redirect();
 
-class blogredirect {
-	function blogredirect( ) {
+class Blog_Redirect {
+
+	function __construct() {
 		add_filter( 'admin_init', array( &$this, 'admin_init' ) );
 		add_action( 'template_redirect', array( &$this, 'template_redirect' ) );
 	}
-	function admin_init( ) {
+
+	function admin_init() {
 		register_setting( 'general', 'blogredirect_url', 'esc_url' );
-		add_settings_field('some_random_text', __('Blog Redirect (301)', 'blogredirect' ), array( &$this, 'add_settings_field'), 'general' ); 
+		add_settings_field('blogredirect_url', __( 'Blog Redirect (301)', 'blogredirect' ), array( &$this, 'add_settings_field' ), 'general' ); 
 	}
-	function add_settings_field( ) {
+
+	function add_settings_field() {
 		$url = get_option( 'blogredirect_url' );
 		echo "<input type='url' name='blogredirect_url' value='{$url}' size='35' />";
 	}
-	function template_redirect( ) {
+
+	function template_redirect() {
 		if ( $url = get_option( 'blogredirect_url' ) ) {
 		
 			$curr = (is_ssl() ? 'https://' : 'http://') . $_SERVER[ 'SERVER_NAME' ] . $_SERVER[ 'REQUEST_URI' ];
 			$root = substr($curr, 0, strpos($curr, '/', 10)) .'/' ;
 
 			$url = trailingslashit( $url );
-			// header('Location:'.str_replace( $root, $url, $curr ), true, 301);
 			wp_redirect( str_replace( $root, $url, $curr ), 301 );
 			exit;
 		}
 	}
+
 }
